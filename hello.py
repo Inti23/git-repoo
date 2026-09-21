28c78026ff3ba26b36999e8bc14b8b04d9c3c4cb
Inti23 <154803478+Inti23@users.noreply.github.com>
2026-09-21 15:57:16 +0500

    Refactor print statements in hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 print("Hello World")
 
+
 print("Hi, I'm Intizar today we are going to learn about Python programming language. Python is a high-level, interpreted programming language known for its simplicity and readability. It is widely used in web development, data analysis, artificial intelligence, scientific computing, and more. Let's get started with some basic concepts and examples in Python!")
+
+
+print("Hi there")
