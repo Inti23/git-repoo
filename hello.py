9613d21692b884853431c88a7588a1cce2de67b8
Intizar Hussain <232010@students.au.edu.pk>
2026-09-21 15:54:22 +0500

    Add introductory print statement about Python programming

--- hello.py
+++ hello.py
@@ -1,1 +1,3 @@
 print("Hello World")
+
+print("Hi, I'm Intizar today we are going to learn about Python programming language. Python is a high-level, interpreted programming language known for its simplicity and readability. It is widely used in web development, data analysis, artificial intelligence, scientific computing, and more. Let's get started with some basic concepts and examples in Python!")
